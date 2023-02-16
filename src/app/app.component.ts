b419d2a7249045f5be329b72055fb2724260bbc6
Johancookies <johan.mancilla@bodytechcorp.com>
2023-02-15 19:23:44 -0500

    state added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'looks';
+  pokemonName: string = 'Pokemon';
+  pokemonNumber: string = '69';
+  pokemonImage: string = '';
+
+  searchPokemon() {
+    this.pokemonName = 'Glaceon';
+    this.pokemonNumber = '471';
+    this.pokemonImage =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/471.png';
+  }
 }
